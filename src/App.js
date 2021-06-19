37c6141fa33444f9564299482edde975cd8fc6f5
JAEGEUN SONG <76800974+drow724@users.noreply.github.com>
2021-06-19 19:35:43 +0900

    이벤트에서 state 변경하기

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ import Subject from './components/Subject';
 class App extends Component {
   constructor(props){
     super(props);
-    this.welcome = {
-      mode:'welcome',
+    this.state = {
+      Mode:'read',
       Subject:{title:'WEB', sub:'World Wide Web!'},
-      welcome:{title:'welcome', desc: 'Hello, React!!'},
+      Welcome:{title:'welcome', desc: 'Hello, React!!'},
       Content:[
         {id:1, title:'HTML', desc:'HTML is HyperText...'},
         {id:2, title:'CSS', desc:'CSS is for design...'},
@@ -22,19 +22,30 @@ class App extends Component {
   render() {
     
     var _title, _desc = null;
-    if(this.state.mode === 'welcome'){
-      _title = this.state.welcome.title;
-      _desc = this.state.welcome.desc;
-    } else if(this.state.mode === 'read') {
-      _title = this.state.contents[0].title;
-      _desc = this.state.contents[0].desc;
+    if(this.state.Mode === 'welcome'){
+      _title = this.state.Welcome.title;
+      _desc = this.state.Welcome.desc;
+    } else if(this.state.Mode === 'read') {
+      _title = this.state.Content[0].title;
+      _desc = this.state.Content[0].desc;
     }
     return (
       <div className="App">
-        <Subject 
+        {/* <Subject 
         title={this.state.Subject.title}
         sub={this.state.Subject.sub}>
-        </Subject>
+        </Subject> */}
+         <header>
+          <h1><a href="/" onClick={function(e){
+            console.log(e);
+            e.preventDefault();
+            //this.state.Mode = 'welcome';
+            this.setState({
+              Mode:'welcome'
+            })
+      }.bind(this)}>{this.state.Subject.title}</a></h1>
+          {this.state.Subject.sub}
+        </header>
         <TOC data={this.state.Content}></TOC>
         <Content title={_title} desc={_desc}></Content>
       </div>
